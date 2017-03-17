8f29528f8f161f325b1c8d8502531186e8f12b40
wagnerrose <wagner_rose@yahoo.com.br>
2017-03-17 17:35:14 +0000

    Inserido Menu

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,15 @@ Rails.application.routes.draw do
   resources :analistas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  get 'viabilidade' => 'vt#index'
-  get 'pop' => 'pop#index'
-  get 'justificativas' => 'justificativas#index'
-  get 'localidades' => 'localidades#index'
-  get 'servicos' => 'servicos#index'
-  get 'uf' => 'uf#index'
-  get 'regionais' => 'regionais#index'
-  get 'empresas' => 'empresas#index'
-  get 'analistas' => 'analistas#index'
+  get 'inicio' => 'welcome#index'
+  # get 'viabilidade' => 'viabilidade#index'
+  # get 'pop' => 'pop#index'
+  # get 'justificativas' => 'justificativas#index'
+  # get 'localidades' => 'localidades#index'
+  # get 'servicos' => 'servicos#index'
+  # get 'uf' => 'uf#index'
+  # get 'regionais' => 'regionais#index'
+  # get 'empresas' => 'empresas#index'
+  # get 'analistas' => 'analistas#index'
   
 end
