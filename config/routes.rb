8539f3323390b1304f9cfbe48fd8459164845ed3
wagnerrose <wagner_rose@outlook.com.br>
2017-09-08 15:35:04 -0300

    backup migracao c9 08Set17

--- config/routes.rb
+++ config/routes.rb
@@ -18,31 +18,15 @@ Rails.application.routes.draw do
       resources :servicos
       resources :telefones
       resources :uf
-      resources :admin, except: [:show]
-  end
+      resources :admin, except: [:show, :destroy]
+    end
 
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-#  devise_for :usuarios
-#  devise_for :admin, :skip => [:registrations]
-
-  devise_for :admin, :class_name => 'Admin', :controllers =>
-    { :registrations => 'backoffice/admin/registrations',
-      :sessions => 'backoffice/admin/sessions',
-      :passwords => 'backoffice/admin/passwords'} do
-    get "admin/sign_up" => "backoffice/admin/registrations#new", :as => :admin_signup
-    get  "admin/sign_in" => "backoffice/admin/sesseion#new", :as => :admin_signin
-  end
-  devise_for :usuario, :class_name => 'Usuario', :controllers =>
-    { :registrations => 'backoffice/usuarios/registrations',
-      :sessions => 'backoffice/usuarios/sessions',
-      :passwords => 'backoffice/usuarios/passwords'} do
-    get "admin/sign_up" => "backoffice/usuarios/registrations#new", :as => :usuarios_signup
-    get  "admin/sign_in" => "backoffice/usuarios/sesseion#new", :as => :usuarios_signin
-  end
-
+  devise_for :usuarios
+  devise_for :admin, :skip => [:registrations]
 
 
   root 'site/home#index'
