c221f34667fd1302fafe388f5b1ecb2e6f4dac62
wagnerrose <wagner_rose@outlook.com.br>
2017-09-08 07:34:29 -0300

    backup 08Set17

--- config/routes.rb
+++ config/routes.rb
@@ -25,12 +25,26 @@ Rails.application.routes.draw do
     get 'home', to: 'home#index'
   end
 
-  devise_for :usuarios
+#  devise_for :usuarios
 #  devise_for :admin, :skip => [:registrations]
-  devise_for :admin, :class_name => 'Admin', :controllers => {:registrations => "backoffice/admin", :sessions => 'backoffice/admin'} do
-    get "admin/sign_up" => "backoffice/registrations#new", :as => :admin_signup
+
+  devise_for :admin, :class_name => 'Admin', :controllers =>
+    { :registrations => 'backoffice/admin/registrations',
+      :sessions => 'backoffice/admin/sessions',
+      :passwords => 'backoffice/admin/passwords'} do
+    get "admin/sign_up" => "backoffice/admin/registrations#new", :as => :admin_signup
     get  "admin/sign_in" => "backoffice/admin/sesseion#new", :as => :admin_signin
   end
+  devise_for :usuario, :class_name => 'Usuario', :controllers =>
+    { :registrations => 'backoffice/usuarios/registrations',
+      :sessions => 'backoffice/usuarios/sessions',
+      :passwords => 'backoffice/usuarios/passwords'} do
+    get "admin/sign_up" => "backoffice/usuarios/registrations#new", :as => :usuarios_signup
+    get  "admin/sign_in" => "backoffice/usuarios/sesseion#new", :as => :usuarios_signin
+  end
+
+
+
   root 'site/home#index'
 
   resources :solicitacoes
